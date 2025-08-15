06dd103e57cdb6e162065c14dcf1e8547caa3d3a
M Karthikeyan <amariyappan.karthikeyan@gmail.com>
2025-08-15 18:36:03 +0530

    Adding Cors

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 const express = require('express');
+const cors = require('cors'); // ✅ Import cors
 const app = express();
 const pool = require('./config/db');
 require('dotenv').config();
-
 const { createUsersTable } = require('./config/Tables/CreateUserTable');
 
 // Middleware
 app.use(express.json());
+app.use(cors()); // ✅ Enable CORS for all routes
 
 // Routes
 app.use('/api/v_1/users', require('./routes/userRoutes'));
-
 
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
+// Test DB connection
 pool.query('SELECT NOW()', (err, result) => {
   if (err) {
     console.error('❌ Database connection failed:', err);
